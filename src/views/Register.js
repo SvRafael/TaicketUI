6f313cab530dae92dde83af399162e3ae631caed
Rafael <rafaelsvsouza@hotmail.com>
2020-05-19 22:33:00 -0300

    Inicia cadastro

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,5 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const Register = () => <h1>Login</h1>
+import { Container, Form, Button } from "react-bootstrap";
 
-export default Register
+const Register = () => {
+  return (
+    <Container>
+      <p className="h1 my-4">Register</p>
+
+      <Form>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Informe seu email" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Informe sua senha" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" block>
+          Entrar
+        </Button>
+
+        <Button variant="secondary" block>
+          Cadastrar
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Register;
